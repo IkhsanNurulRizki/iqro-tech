55851ce95f3e50e0003138bbbda3ce93c8028102
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-07-30 11:21:31 +0700

    update

--- src/Components/Langkah.js
+++ src/Components/Langkah.js
@@ -4,40 +4,40 @@ export class Langkah extends Component{
     render(){
         return(
 // <!-- ======= Step info Section ======= -->
-    <section id="featured-services" class="featured-services">
-        <div class="container" data-aos="fade-up">
+    <section id="featured-services" className="featured-services">
+        <div className="container" data-aos="fade-up">
 
-        <div class="row">
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-                <div class="icon"></div>
-                <h4 class="title"><a href="">TAHAP 1</a></h4>
-                <p class="description">Kamu bisa memilih terlebih dahulu bacaan yang akan kamu ucapkan dengan memilih juz, surah, dan juga ayat.</p>
+        <div className="row">
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="100">
+                <div className="icon"></div>
+                <h4 className="title"><a href="">TAHAP 1</a></h4>
+                <p className="description">Kamu bisa memilih terlebih dahulu bacaan yang akan kamu ucapkan dengan memilih juz, surah, dan juga ayat.</p>
             </div>
             </div>
 
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-                <div class="icon"></div>
-                <h4 class="title"><a href="">TAHAP 2</a></h4>
-                <p class="description">Kemudian kamu dapat mengucapkan bacaan yang sudah kamu pilih dengan menekan tombol "Mulai Mengaji".
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="200">
+                <div className="icon"></div>
+                <h4 className="title"><a href="">TAHAP 2</a></h4>
+                <p className="description">Kemudian kamu dapat mengucapkan bacaan yang sudah kamu pilih dengan menekan tombol "Mulai Mengaji".
                 </p>
             </div>
             </div>
 
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-                <div class="icon"></div>
-                <h4 class="title"><a href="">TAHAP 3</a></h4>
-                <p class="description">Setelah teks muncul, kamu cukup tekan "Cek Bacaan" untuk mengecek bacaanmu.</p>
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="300">
+                <div className="icon"></div>
+                <h4 className="title"><a href="">TAHAP 3</a></h4>
+                <p className="description">Setelah teks muncul, kamu cukup tekan "Cek Bacaan" untuk mengecek bacaanmu.</p>
             </div>
             </div>
 
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
-                <div class="icon"></div>
-                <h4 class="title"><a href="">SELESAI</a></h4>
-                <p class="description">Setelah proses pengecekan selesai, kamu dapat mendengarkan audio lantunan Al-Quran dengan menekan 
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="400">
+                <div className="icon"></div>
+                <h4 className="title"><a href="">SELESAI</a></h4>
+                <p className="description">Setelah proses pengecekan selesai, kamu dapat mendengarkan audio lantunan Al-Quran dengan menekan 
                 tombol "Dengarkan", Kamu bisa melakukan pengecekan ini secara berulang-ulang dengan menekan tombol
                 "Memulai ulang" dan kamu bisa lakukan kembali dari tahap awal.</p>
             </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,18 +3,22 @@ import React, { Component } from "react";
 export class Navbar extends Component {
   render() {
     return (
-      <div className="container d-flex align-items-center justify-content-between">
-        <img style={{ width: '150px' }} src="./pic/logo.png" alt="logo" />
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li><a className="nav-link scrollto active" href="#hero">Sekilas web</a></li>
-            <li><a className="nav-link scrollto" href="#featured-services">Tahapan</a></li>
-            <li><a className="nav-link scrollto" href="#inti">Mulai disini!</a></li>
-            <li><a className="nav-link scrollto" href="#about">Tentang IqroTech</a></li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
-        </nav> {/*<!-- .navbar --> */}
-      </div>
+      <header id="header" className="d-flex align-items-center">
+  <div className="container d-flex align-items-center justify-content-between">
+    <img style={{ width: '150px' }} src="./pic/logo.png" alt="logo"/>
+      <nav id="navbar" className="navbar">
+        <ul>
+          <li><a className="nav-link scrollto active" href="#hero">Sekilas web</a></li>
+          <li><a className="nav-link scrollto" href="#featured-services">Tahapan</a></li>
+          <li><a className="nav-link scrollto" href="#inti">Mulai disini!</a></li>
+          <li><a className="nav-link scrollto" href="#about">Tentang IqroTech</a></li>
+        </ul>
+        <i className="bi bi-list mobile-nav-toggle"></i>
+    </nav>
+    {/* <!-- End navbar --> */}
+  </div>
+</header>
+// <!-- End Header -->
     )
   }
 }
